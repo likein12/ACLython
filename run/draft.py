8190530b658df4d5420b6c644792240deed1172d
likein12 <deoxyglucose3@gmail.com>
2020-09-17 21:04:30 +0900

    change

--- run/draft.py
+++ run/draft.py
@@ -1,6 +1,14 @@
-from atcoder import SuffixArray
+from atcoder import SccGraph, readInt
 
 def main():
-    print(*SuffixArray(input()))
+    N, M = readInt(2)
+    sg = SccGraph(N)
+    for i in range(M):
+        a, b = readInt(2)
+        sg.add_edge(a, b)
+    ans = sg.scc()
+    print(len(ans))
+    for i in range(len(ans)):
+        print(len(ans[i]),*ans[i])
 
 main()
